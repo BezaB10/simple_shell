2c2d504268316b65eb5da91f549ce54325f26d4b
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-05-18 13:19:37 -0700

    tokenizer

--- tokenizer.c
+++ tokenizer.c
@@ -1,39 +1,104 @@
 #include "shell.h"
 
+int token_len(char *str, char *delim);
+int count_tokens(char *str, char *delim);
+char **_strtok(char *line, char *delim);
+
 /**
- * tokenize - tokenizes a stirng
- * @lineptr: what the user inputed
- * Return: a ptr to arr of ptrs
+ * token_len - Locates the delimiter index marking the end
+ * of the first token contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
+ *
+ * Return: The delimiter index marking the end of
+ *         the intitial token pointed to be str.
  */
-
-char **tokenize(char *lineptr)
+int token_len(char *str, char *delim)
 {
-	char **user_command = NULL;
-	char *token = NULL;
-	size_t i = 0;
-	int size = 0;
+	int index = 0, len = 0;
 
-	if (lineptr == NULL)
-		return (NULL);
-
-	for (i = 0; lineptr[i]; i++)
+	while (*(str + index) && *(str + index) != *delim)
 	{
-		if (lineptr[i] == ' ')
-			size++;
+		len++;
+		index++;
 	}
-	if ((size + 1) == _strlen(lineptr))
-		return (NULL);
-	user_command = malloc(sizeof(char *) * (size + 2));
-	if (user_command == NULL)
-		return (NULL);
 
-	token = strtok(lineptr, " \n\t\r");
+	return (len);
+}
 
-	for (i = 0; token != NULL; i++)
+/**
+ * count_tokens - Counts the number of delimited
+ *                words contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
+ *
+ * Return: The number of words contained within str.
+ */
+int count_tokens(char *str, char *delim)
+{
+	int index, tokens = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
 	{
-		user_command[i] = token;
-		token = strtok(NULL, " \n\t\r");
+		if (*(str + index) != *delim)
+		{
+			tokens++;
+			index += token_len(str + index, delim);
+		}
 	}
-	user_command[i] = NULL;
-	return (user_command);
+
+	return (tokens);
+}
+
+/**
+ * _strtok - Tokenizes a string.
+ * @line: The string.
+ * @delim: The delimiter character to tokenize the string by.
+ *
+ * Return: A pointer to an array containing the tokenized words.
+ */
+char **_strtok(char *line, char *delim)
+{
+	char **ptr;
+	int index = 0, tokens, t, letters, l;
+
+	tokens = count_tokens(line, delim);
+	if (tokens == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(char *) * (tokens + 2));
+	if (!ptr)
+		return (NULL);
+
+	for (t = 0; t < tokens; t++)
+	{
+		while (line[index] == *delim)
+			index++;
+
+		letters = token_len(line + index, delim);
+
+		ptr[t] = malloc(sizeof(char) * (letters + 1));
+		if (!ptr[t])
+		{
+			for (index -= 1; index >= 0; index--)
+				free(ptr[index]);
+			free(ptr);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+		{
+			ptr[t][l] = line[index];
+			index++;
+		}
+
+		ptr[t][l] = '\0';
+	}
+	ptr[t] = NULL;
+	ptr[t + 1] = NULL;
+
+	return (ptr);
 }
